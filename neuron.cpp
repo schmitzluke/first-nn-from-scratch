91fed412ddc30613e4b01a77d93f349c7e0b72a4
schmitzluke <141272376+schmitzluke@users.noreply.github.com>
2026-01-16 22:03:25 +0100

    feat: flexible amount of inputs and weights for a neuron by implementing std::vectors

--- neuron.cpp
+++ neuron.cpp
@@ -1,5 +1,6 @@
 #include "neuron.h"
 
 
-Neuron::Neuron(double height, double weight, double shoeSize,double w_1, double w_2, double w_3, double b):
-    m_height(height),m_weight(weight), m_shoeSize(shoeSize), m_w1(w_1), m_w2(w_2),m_w3(w_3), m_b(b){}
+
+Neuron::Neuron(std::vector<double> inputs, std::vector<double> weights,double b):
+    m_inputs(inputs), m_weights(weights), m_b(b){}
